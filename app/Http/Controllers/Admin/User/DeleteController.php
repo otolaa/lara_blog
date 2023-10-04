73835500e7d1f1499df707d6583597b330d17e9c
otolaa <kalabunga82@gmail.com>
2023-10-04 20:09:39 +0200

    👽

---
+++ app/Http/Controllers/Admin/User/DeleteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+
+class DeleteController extends Controller
+{
+    public function __invoke(User $user)
+    {
+        $user->delete();
+        return redirect()->route('admin.user.index');
+    }
+}
