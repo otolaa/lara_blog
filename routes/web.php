ae60d5eda8304d4036cea86e918b43191b5cfe74
otolaa <kalabunga82@gmail.com>
2023-11-30 22:36:55 +0200

    👽

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Main\IndexController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
